749cb8217873ed009c55cbac2698152017c0f6b7
Elizabeth Klymova <lozaklimovaaadd@gmail.com>
2024-04-02 11:43:22 +0300

    prepared base structure for project item

--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -1,4 +1,45 @@
-const ProjectItem = () => {
+const ProjectItem = ({
+  project: {
+    projectName,
+    pageLink,
+    links,
+    title,
+    role,
+    description,
+    technologies,
+  },
+}) => {
+  // {
+  //   "id": "1",
+  //   "projectName": "Task Pro",
+  //   "pageLink": "https://lizaklimova.github.io/task-pro/",
+  //   "links": [
+  //     {
+  //       "text": "GitHub (FE)",
+  //       "link": "https://github.com/lizaklimova/task-pro"
+  //     },
+  //     {
+  //       "text": "GitHub (BE)",
+  //       "link": "https://github.com/chanankuan/task-pro-rest-api"
+  //     }
+  //   ],
+  //   "title": "Project management app",
+  //   "role": "Team Lead – Developer",
+  //   "description": "TaskPro is an application designed for efficient task and project management. It provides a convenient way to organize tasks and collaborate with teams. Users can create boards to represent various projects or work areas, with the flexibility to add cards for tracking progress and priorities. Key features include the ability to drag and drop cards between columns for seamless workflow organization. Moreover, TaskPro introduces enhanced functionalities such as task statistics categorized by deadline and priority, along with a task calendar for improved scheduling",
+  //   "technologies": [
+  //     "React.js",
+  //     "Redux Toolkit",
+  //     "Redux Persist",
+  //     "Styled components",
+  //     "NodeJS",
+  //     "Express",
+  //     "Cloudinary",
+  //     "MongoDB",
+  //     "Mongoose",
+  //     "Google OAuth",
+  //     "JWT"
+  //   ]
+  // },
   return <div></div>;
 };
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,13 @@
 import projectsData from "data/projects.json";
+import ProjectItem from "components/ProjectItem";
 
 function Projects() {
   return (
     <ul>
       {projectsData.map((project) => (
-        <li></li>
+        <li key={project.id}>
+          <ProjectItem project={project} />
+        </li>
       ))}
     </ul>
   );
